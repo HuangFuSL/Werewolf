33bb681c4cd851568492a3266207f7d29710493a
hearts-six <63958367+hearts-six@users.noreply.github.com>
2020-11-08 23:53:08 +0800

    Update server.py

--- Werewolf/server.py
+++ Werewolf/server.py
@@ -17,40 +17,40 @@ SERVER_PORT = 21567 #you can change it
 BUFSIZ = 1024
 ADDR = (HOST,SERVER_PORT)
 
-tcpSerSock = socket(AF_INET,SOCK_STREAM)
-tcpSerSock.bind(ADDR)
-tcpSerSock.listen(5)
+if __name__ == "__main__":
+
+    tcpSerSock = socket(AF_INET,SOCK_STREAM)
+    tcpSerSock.bind(ADDR)
+    tcpSerSock.listen(5)
 
 
-# In[ ]:
-
-
-while True:
-    #first particpant set the number of players
-    print('waiting for first client connection...')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print('...connnecting from:', addr)
-    #??? how to receive the number of players ???
-    N = 12
-    newgame = Game(N,identityList)
-    for i in range(1,N+1):
-        newgame.addPlayer(data)
-    """
-    set connections with clients and distribute identity
-    set corresponding threads
-    """
-    while (newgame.notend()):
-        #for night
-        
-        #for day
 
     while True:
-        data = tcpCliSock.recv(BUFSIZ)
-        if not data:
-            break
-        #tcpCliSock.send('[%s] %s' %(bytes(ctime(),'utf-8'),data))
-        tcpCliSock.send(('[%s] %s' % (ctime(), data)).encode())
-    tcpCliSock.close()
-tcpSerSock.close()
+        #first particpant set the number of players
+        print('waiting for first client connection...')
+        tcpCliSock, addr = tcpSerSock.accept()
+        print('...connnecting from:', addr)
+        #??? how to receive the number of players ???
+        N = 12
+        newgame = Game(N,identityList)
+        for i in range(1,N+1):
+            newgame.addPlayer(data)
+        """
+        set connections with clients and distribute identity
+        set corresponding threads
+        """
+        while (newgame.notend()):
+            #for night
+
+            #for day
+
+        while True:
+            data = tcpCliSock.recv(BUFSIZ)
+            if not data:
+                break
+            #tcpCliSock.send('[%s] %s' %(bytes(ctime(),'utf-8'),data))
+            tcpCliSock.send(('[%s] %s' % (ctime(), data)).encode())
+        tcpCliSock.close()
+    tcpSerSock.close()
 
 
