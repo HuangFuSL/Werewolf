2ab52634db1ce9c453621fc9239f8a5fb64a9f89
hearts-six <63958367+hearts-six@users.noreply.github.com>
2020-11-11 15:07:21 +0800

    Add files via upload

--- Werewolf/server.py
+++ Werewolf/server.py
@@ -1,56 +1,84 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from socket import *
 from time import ctime
-from abstraction import *
-from random import shuffle, randint
-from WP import ChunckedData
 from threading import Thread
+
+from server.logic import Game
 from util import getIdentityCode, getBasePacket
+from random import shuffle, randint
+from abstraction import *
+from WP import api
+from server import logic
+
 
 HOST = ''
-SERVER_PORT = 21567 #you can change it
+SERVER_PORT = 21567
 BUFSIZ = 1024
 ADDR = (HOST,SERVER_PORT)
 
-if __name__ == "__main__":
+tcpSerSock = socket(AF_INET,SOCK_STREAM)
 
-    tcpSerSock = socket(AF_INET,SOCK_STREAM)
+#input the number of clients
+playerCount = input('>')
+while(not(type(playerCount) == int and playerCount >= 6 and playerCount <= 12)):
+    print('Please input the right number between 6 and 12!')
+    playerCount = input('>')
+print('the number of players is: %d' % playerCount)
+
+#wait for client connection and create the connection
+game = Game(playerCount)
+for idclient in range(1, int(playerCount) + 1):
     tcpSerSock.bind(ADDR)
     tcpSerSock.listen(5)
+    print('waiting for client %d connection...' % idclient)
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('...connnecting client %d from:'% idclient, addr)
+    game.addAddr(addr[1], addr[0])
 
+#allocate the identity and tell to the corresponding client
+game.allocateIdentity(ADDR)
 
+#if the game not ends
 
-    while True:
-        #first particpant set the number of players
-        print('waiting for first client connection...')
-        tcpCliSock, addr = tcpSerSock.accept()
-        print('...connnecting from:', addr)
-        #??? how to receive the number of players ???
-        N = 12
-        newgame = Game(N,identityList)
-        for i in range(1,N+1):
-            newgame.addPlayer(data)
-        """
-        set connections with clients and distribute identity
-        set corresponding threads
-        """
-        while (newgame.notend()):
-            #for night
+    #prompt for action of werewolves
 
-            #for day
+    #prompt for action of witch
 
-        while True:
-            data = tcpCliSock.recv(BUFSIZ)
-            if not data:
-                break
-            #tcpCliSock.send('[%s] %s' %(bytes(ctime(),'utf-8'),data))
-            tcpCliSock.send(('[%s] %s' % (ctime(), data)).encode())
-        tcpCliSock.close()
-    tcpSerSock.close()
+    #prompt for action of predictor
 
+    #prompt for action of guard
 
+    #if is the first night
+
+        #prompt for the election of police
+
+        #the candidate talks
+
+        #vote
+
+        #announce the police
+
+    #announce the victim
+
+    #prompt and wait for the conversation
+
+    #vote
+
+    #announce the exile
+
+"""
+set connections with clients and distribute identity
+set corresponding threads
+"""
+"""while (newgame.notend()):
+    #for night
+
+    #for day
+
+while True:
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    #tcpCliSock.send('[%s] %s' %(bytes(ctime(),'utf-8'),data))
+    tcpCliSock.send(('[%s] %s' % (ctime(), data)).encode())
+tcpCliSock.close()
+tcpSerSock.close()"""
