0096310f2a2c90d10d1005a50ae5a8079985c168
HuangFuSL <huangfusl@outlook.com>
2020-11-11 11:47:12 +0800

    Resolved syntax errors

--- Werewolf/server/logic.py
+++ Werewolf/server/logic.py
@@ -1,12 +1,13 @@
-from abstraction import *
+from ..abstraction import *
 from random import shuffle, randint
-from WP import ChunckedData
+from ..WP import ChunckedData
 from threading import Thread
-from util import getIdentityCode, getBasePacket
+from ..util import getIdentityCode, getBasePacket
+
 
 class Game():
 
-    def __init__(playerCount: int, identityList: list):
+    def __init__(self, playerCount: int, identityList: list):
         self.playerCount = playerCount
         self.activePlayer = {}
         self.ports = list(range(50000, 50000 + playerCount + 1))
@@ -23,32 +24,36 @@ class Game():
     def addPlayer(self, data: ChunckedData):
         assert self.running == False
         assert data.content['type'] == 2
-        assert len(identityList) != 0
+        assert len(self.identityList) != 0
         server = data.getAddr('destination')
         client = data.getAddr('source')
         id = data.content['chosenSeat']
         if id in range(1, self.playerCount + 1) and id not in self.activePlayer.keys():
-            self.activePlayer[id] = self.identityList.pop()(id=id, server=server, client=client)
+            self.activePlayer[id] = self.identityList.pop()(
+                id=id, server=server, client=client)
         else:
             id = randint(1, self.playerCount)
             while id in self.activate.keys():
                 id = randint(1, self.playerCount)
-            self.activePlayer[id] = self.identityList.pop()(id=id, server=server, client=client)
+            self.activePlayer[id] = self.identityList.pop()(
+                id=id, server=server, client=client)
         identity = getIdentityCode(self.activePlayer[id])
         packet = getBasePacket(server, client)
         packet['success'] = True
         packet['chosenSeat'] = id
         packet['identity'] = identity
-        sendingThread = Thread(target=ChunckedData(-2, **packet).send(), args=(self.activePlayer[id].socket, ))
+        sendingThread = Thread(
+            target=ChunckedData(-2, **packet).send(), args=(self.activePlayer[id].socket, ))
         sendingThread.start()
+
 
 class IncomingConnection(Thread):
 
-    def __init__(self, addr: tuple, dest: Game,id: int):
-        super(EstablishConnThread, self).__init__()
+    def __init__(self, addr: tuple, dest: Game, id: int):
+        super(IncomingConnection, self).__init__()
         self.result = None
         self.address = addr
         self.id = id
 
     def run(self):
-        
+        pass
