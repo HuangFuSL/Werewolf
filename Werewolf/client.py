b283db73ba761fc818241ffe43ea8f905d45eca4
HuangFuSL <huangfusl@outlook.com>
2020-11-11 12:13:43 +0800

    Partly resolved syntax error

--- Werewolf/client.py
+++ Werewolf/client.py
@@ -8,27 +8,29 @@ Created on Sat Nov  7 15:06:28 2020
 import socket
 import time
 from threading import Thread
-from .api import *
+from .WP import ChunckedData, _recv
 
 HOST = '127.0.0.1'
 PORT = 21567
 BUFSIZE = 1024
-ADDR = (HOST,PORT)
+ADDR = (HOST, PORT)
 
-class IoThread(threading.Thread):
-   def __init__(self, threadID, ChunkedData):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.packet = ChunkedData
 
-   def run(self):
-       '''
-       prprpr777's code
-       '''
+class IoThread(Thread):
+    def __init__(self, threadID, ChunkedData):
+        super().__init__(self)
+        self.threadID = threadID
+        self.packet = ChunkedData
+
+    def run(self):
+        '''
+        prprpr777's code
+        '''
+
 
 class Player():
     def __init__(self):
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(ADDR)
 
     def _getBasePacket(self) -> dict:
@@ -38,31 +40,31 @@ class Player():
         ret['destAddr'] = self.server[0]
         ret['destPort'] = self.server[1]
         return ret
-    
-    def _register(self)
+
+    def _register(self):
         packet = self._getBasePacket()
         packetSend = ChunckedData(1, **packet)
-        packetSend.send(self.socket,ADDR)
+        packetSend.send(self.socket, ADDR)
         regInfo = _recv(self.socket)
         self.id = regInfo.content['Seat']
         self.role = regInfo.content['identity']
         print("Your identity is %d/n " % self.role)
 
         self.police = False
-        self.alive = True            
+        self.alive = True
 
-    def playing():
+    def playing(self):
         k = 0
         try:
             while(1):
                 reqPacket = _recv(self.socket)
                 k += 1
-                IoThread(k,reqPacket)
+                IoThread(k, reqPacket)
         except KeyboardInterrupt:
-            if player.role != 1:   #我这里假定1是狼人了，没找到各角色对应的序号，以后再改叭
+            if player.role != 1:  # 我这里假定1是狼人了，没找到各角色对应的序号，以后再改叭
                 pass
             else:
                 packet = self._getBasePacket()
                 packetSend = ChunckedData(1, **packet)
-                packetSend.send(self.socket,ADDR)
+                packetSend.send(self.socket, ADDR)
                 self.alive = False
