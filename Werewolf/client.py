928b58f675e91801614c559dda88ff011a49d774
HuangFuSL <huangfusl@outlook.com>
2020-11-15 22:36:50 +0800

    2020/11/15 Milestone

--- Werewolf/client.py
+++ Werewolf/client.py
@@ -8,10 +8,12 @@ Created on Sat Nov  7 15:06:28 2020
 import socket
 import time
 from threading import Thread
+from typing import Optional
 from .WP import ChunckedData, _recv
 
 BUFSIZE = 1024
 ROLE = 0
+
 
 class ReceiveTimeoutError(Exception):
 
@@ -22,102 +24,110 @@ class ReceiveTimeoutError(Exception):
     def __str__(self):
         return "Data receive timeout."
 
+
 class IoThread(Thread):
     def __init__(self, ChunkedData, ADDR):
         super().__init__()
-        self.packet = ChunkedData
+        self.packet: ChunckedData = ChunkedData
         self.ADDR = ADDR
 
     def run(self):
-        type = (int)ret.packetType
-
+        type = int(self.packet.type)
+        ret: Optional[ChunckedData] = None
         if (type == -1):
             """
             -1是服务器发出的建立连接的信息
             """
-            print("%d seat(s) remained and the seats you can choose are :" %(ret.content['playerRemaining']))
-            for x in ret.content['playerSeats']:
+            print("%d seat(s) remained and the seats you can choose are :" %
+                  (self.packet.content['playerRemaining']))
+            for x in self.packet.content['playerSeats']:
                 print(x, end=' ')
             print("Please enter the number of seat you choose: ")
             chosenSeat = input()
-            ret = ChunckedData(2,chosenSeat = seatChosed)
-    
+            ret = ChunckedData(2, chosenSeat=chosenSeat)
+
         elif(type == -2):
             """
             -2是服务器发出的选座信息，不需要回复
             """
-            if (ret.content['success'] == True):
+            if (self.packet.content['success'] == True):
                 print("Successfully chose the seat! ")
-            else
+            else:
                 print("The seat you chose was taken.")
-    
-            print("your seat is %d and your identity is %d" %(ret.content['chosenSeat'],ret.content['identity']))
+
+            print("your seat is %d and your identity is %d" %
+                  (self.packet.content['chosenSeat'], self.packet.content['identity']))
 
         elif(type == 3):
             """
             3是服务器发出的请求玩家执行的操作
             """
             print("the format for you to enter is ")
-            print(ret.content['format'])
+            print(self.packet.content['format'])
             print('\n')
-            print(ret.content['prompt'])
-            print("the time limit is %d" %(ret.content['timeLimit']))
-    
-            ReceiveTimeoutError(timeout=ret.content['timeLimit'])
-    
+            print(self.packet.content['prompt'])
+            print("the time limit is %d" % (self.packet.content['timeLimit']))
+
+            ReceiveTimeoutError(timeout=self.packet.content['timeLimit'])
+
             target = input()
-            ret = ChunckedData(-3,target = target)
+            if self.packet.content['format'] == 'str':
+                ret = ChunckedData(-3, target=target)
+            else:
+                ret = ChunckedData(-3, target=eval(target))
 
         elif(type == 4):
             """
             4是服务器公布的信息，-4 response
             不太确定这个packet中的parameter是啥意思，就先打出来了
             """
-            print(ret.content['description'])
-            print(ret.content['parameter'])
-    
-            ret = ChunckedData(-4, ACK = True)
-    
+            print(self.packet.content['description'])
+            print(self.packet.content['parameter'])
+
+            ret = ChunckedData(-4, ACK=True)
+
         elif(type == 5):
             """
             自由发言阶段，全部转发显示
             """
-            print(ret.content['content'])
+            print(self.packet.content['content'])
 
         elif(type == 6):
             '''
             限制时间发言阶段
             '''
-            print("the time for you to speak is %d" %(ret.content['timeLimit']))
-            ReceiveTimeoutError(timeout=ret.content['timeLimit'])
+            print("the time for you to speak is %d" %
+                  (self.packet.content['timeLimit']))
+            ReceiveTimeoutError(timeout=self.packet.content['timeLimit'])
             content = input()
 
-            ret = ChunckedData(-6, content = content)
+            ret = ChunckedData(-6, content=content)
 
         elif(type == 7):
             '''
             投票阶段
             '''
-            print(ret.content['prompt'])
+            print(self.packet.content['prompt'])
             candidate = input()
 
             if (candidate.isspace() == True):
                 ret = ChunckedData(-7, vote=False, candidate=0)
             else:
                 ret = ChunckedData(-7, vote=True, candidate=candidate)
-    
+
         elif(type == 8):
             pass
-        
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ret.send(self, self.socket, ADDR)
-        
+
+        if ret:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ret.send(self.socket, self.ADDR)
+
 
 class Player():
     def __init__(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ADDR = (host, post)
-        self.socket.connect(ADDR)
+        self.ADDR = (host, port)
+        self.socket.connect(self.ADDR)
 
     def _getBasePacket(self) -> dict:
         ret = {}
@@ -128,10 +138,10 @@ class Player():
         return ret
 
     def _register(self):
-        global ADDR, ROLE
+        global ROLE
         packet = self._getBasePacket()
         packetSend = ChunckedData(1, **packet)
-        packetSend.send(self.socket, ADDR)
+        packetSend.send(self.socket, self.ADDR)
         regInfo = _recv(self.socket)
         self.id = regInfo.content['Seat']
         ROLE = regInfo.content['identity']
@@ -151,8 +161,8 @@ class Player():
             # 编号：
             #   村民：0
             #   狼人：-1
-            #   狼王：-3
             #   白狼王：-2
+            #   狼王：-3
             #   预言家：1
             #   女巫：2
             #   猎人：3
@@ -163,5 +173,5 @@ class Player():
             else:
                 packet = self._getBasePacket()
                 packetSend = ChunckedData(1, **packet)
-                packetSend.send(self.socket, ADDR)
+                packetSend.send(self.socket, self.ADDR)
                 self.alive = False
